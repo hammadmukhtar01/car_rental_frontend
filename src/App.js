ae98278a27bdbcf3eac85eb7f7d77a28145e5db6
hammadmukhtar01 <hammad.mukhtar@milele.com>
2024-06-07 10:36:32 +0400

    Adding Build files in project directory and set 404 page error back

--- src/App.js
+++ src/App.js
@@ -60,8 +60,7 @@ const App = () => {
             <Route path="/blogPage2/2" element={<BlogPage2 />} />
             <Route path="/blogPage3/3" element={<BlogPage3 />} />
             <Route path="/404" element={<Page404 />} />
-            {/* <Route path="*" element={<Page404 />} /> */}
-            <Route element={<Page404 />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </div>
       </GlobalLoader>
