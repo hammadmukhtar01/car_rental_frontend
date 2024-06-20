54fcb776d016adb84c7337bd7336c611241253fb
hammadmukhtar01 <hammad.mukhtar@milele.com>
2024-06-20 12:21:13 +0400

    update whatsapp number and set padding for insta feed

--- src/components/Pages/navbar/locationNumberIcons.jsx
+++ src/components/Pages/navbar/locationNumberIcons.jsx
@@ -4,7 +4,7 @@ import { FaPhone, FaMapMarkerAlt, FaWhatsapp } from "react-icons/fa";
 import { Modal } from "react-bootstrap";
 
 const FixedNumLocButtons = () => {
-  const phoneNumber = "0563298330";
+  const phoneNumber = "971563298330";
   const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
 
   const [showModal, setShowModal] = useState(false);
