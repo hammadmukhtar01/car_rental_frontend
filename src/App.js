642134638eca9368c6fa8fade7ac08e6ab29213e
hammadmukhtar01 <hammad.mukhtar@milele.com>
2024-07-02 10:26:45 +0400

    Hide error page route and add null checks for pick drop locations input field

--- src/App.js
+++ src/App.js
@@ -70,8 +70,7 @@ const App = () => {
             <Route path="/blogPage1/1" element={<BlogPage1 />} />
             <Route path="/blogPage2/2" element={<BlogPage2 />} />
             <Route path="/blogPage3/3" element={<BlogPage3 />} />
-            <Route path="/pageNotFound" element={<PageNotFound />} />
-            <Route path="*" element={<PageNotFound />} />
+            {/* <Route path="*" element={<PageNotFound />} /> */}
           </Routes>
         </div>
       </GlobalLoader>
