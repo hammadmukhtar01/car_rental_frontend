22fb613466654e93c23747db4003d20b601784c7
hammad8634 <hammadmukhtar1122@gmail.com>
2024-08-12 10:59:44 +0400

    ewmove default open menu to close in nationality input in booking page

--- src/components/Pages/multipleStepsForm/documentsAndBooking.jsx
+++ src/components/Pages/multipleStepsForm/documentsAndBooking.jsx
@@ -1412,7 +1412,6 @@ const AddOnsDocuments = ({ prevStep, nextStep }) => {
                                     : selectStyles
                                 }
                                 isDisabled={auth && user_token}
-                                menuIsOpen= {true}
                               />
                             </Form.Group>
                           </Col>
